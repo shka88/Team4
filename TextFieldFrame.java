6e2579fc320f657a2403ac91efe2975653e29592
leekayon <lky1732@gmail.com>
2019-12-02 13:11:31 +0900

    Initial sharing of project

---
+++ TextFieldFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+/**
+ * 여기에 TextFieldFrame 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class TextFieldFrame extends JFrame
+{
+    private TextFieldPanel mp;
+    public TextFieldFrame(){
+        this.setTitle("실습_4");
+        this.setSize(250,200);
+        
+        mp = new TextFieldPanel();
+        this.add(mp);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
---
+++ TextFieldPanel.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+/**
+ * 여기에 TextFieldPanel 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class TextFieldPanel extends JPanel
+{
+    JTextField mtfName,mtfDept,mtfAddress;
+    JLabel mlName, mlDept, mlAddress;
+    public TextFieldPanel(){
+        mlName = new JLabel("이름");
+        this.add(mlName);
+        mtfName = new JTextField(" ",20);
+        this.add(mtfName);
+        
+        mlDept = new JLabel("학과");
+        this.add(mlDept);
+        mtfDept = new JTextField("글로벌소프트웨어",20);
+        this.add(mtfDept);
+        
+        mlAddress = new JLabel("주소");
+        this.add(mlAddress);
+        mtfAddress = new JTextField("서울시...",20);
+        this.add(mtfAddress);
+
+    }
+}
